e22007820d61b67b8ae637174bda4ead224facaf
Zhuofan Zhang <zhangzhuofan2019@163.com>
2023-08-30 21:00:34 +0100

    add image captcha verification code

---
+++ xdclass-user-service/src/main/java/com/zzf/controller/NotificationController.java
@@ -0,0 +1,44 @@
+package com.zzf.controller;
+
+
+import com.google.code.kaptcha.Producer;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@Api(tags = "notification")
+@RestController
+@RequestMapping("/api/user/v1")
+@Slf4j
+public class NotificationController {
+
+    @Autowired
+    private Producer captchaProducer;
+
+    @GetMapping("captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
+        String captchaText = captchaProducer.createText();
+        log.info("Text Code:{}",captchaText);
+
+        BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            ImageIO.write(bufferedImage,"jpg",outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e){
+            log.error(e.getMessage());
+        }
+    }
+}
