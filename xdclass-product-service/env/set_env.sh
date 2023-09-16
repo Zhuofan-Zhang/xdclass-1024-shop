e3f061ed096271255533edb8f4a5286eb69186f1
Zhuofan Zhang <zhangzhuofan2019@163.com>
2023-09-16 10:24:27 +0100

    test using env in github workflow

--- xdclass-product-service/env/set_env.sh
+++ xdclass-product-service/env/set_env.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
 cd ..
-echo "\nspring.redis.host: ${ secrets.REDIS_HOST }" >> ./src/main/resources/application.yml
-echo "\nspring.redis.port: ${ secrets.REDIS_PORT }" >> ./src/main/resources/application.yml
-echo "\nspring.redis.password: ${ secrets.REDIS_PASSWORD }" >> ./src/main/resources/application.yml
-echo  "\nspring.datasource.driver-class-name: ${ env.DATABASE_DRIVER }" >> ./src/main/resources/application.yml
-echo "\nspring.datasource.url: ${ secrets.SQL_URL }" >> ./src/main/resources/application.yml
-echo  "\nspring.datasource.username: ${ secrets.DB_USERNAME }" >> ./src/main/resources/application.yml
-echo  "\nspring.datasource.password: ${ secrets.REDIS_PASSWORD }" >> ./src/main/resources/application.yml
+
+
+#!/bin/bash
+
+# Read the value of an environment variable
+#redis_host=$REDIS_HOST
+#redis_port=$REDIS_PORT
+#redis_password=$REDIS_PASSWORD
+#
+## Use the value in your script
+#echo "The value of VARIABLE_NAME is: $my_variable"
+
+
+echo "\nspring.redis.host: $redis_host" >> ./src/main/resources/application.yml
+#echo "\nspring.redis.port: $redis_port" >> ./src/main/resources/application.yml
+#echo "\nspring.redis.password: $redis_password" >> ./src/main/resources/application.yml
+#echo  "\nspring.datasource.driver-class-name: ${ env.DATABASE_DRIVER }" >> ./src/main/resources/application.yml
+#echo "\nspring.datasource.url: ${ secrets.SQL_URL }" >> ./src/main/resources/application.yml
+#echo  "\nspring.datasource.username: ${ secrets.DB_USERNAME }" >> ./src/main/resources/application.yml
+#echo  "\nspring.datasource.password: ${ secrets.REDIS_PASSWORD }" >> ./src/main/resources/application.yml
